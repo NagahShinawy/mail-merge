9d03ea0d0e70f411e9c8ad8620d4655890eeeaf8
Nagah <nagah.shinawy@gmail.com>
2021-06-11 07:34:08 +0300

    refactor and black

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ from mail import Mail
 LETTER_PATH = "./inputs/letters/starting_letter.txt"
 NAMES_PATH = "./inputs/names/names.txt"
 SENDING_MAIL_PATH = "./output/readyToSend"
-
+PLACEHOLDER = "[name]"
 
 if __name__ == "__main__":
     namesf = File(path=NAMES_PATH)
     mailtemplate = File(path=LETTER_PATH).content
 
     for name in namesf:
-        body = mailtemplate.replace("[name]", name)
+        body = mailtemplate.replace(PLACEHOLDER, name)
         mail = Mail(recipient=name, content=body, saved_to=SENDING_MAIL_PATH)
         mail.send()
